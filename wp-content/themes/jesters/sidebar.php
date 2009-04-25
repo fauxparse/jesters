1bfb7eaa80196ae3992932fcdc379bb28a37b3f8
Matt Powell <fauxparse@gmail.com>
2009-04-25 13:45:47 +1200

    adding photos and so on

--- wp-content/themes/jesters/sidebar.php
+++ wp-content/themes/jesters/sidebar.php
@@ -8,6 +8,7 @@
 <div id="sidebar" class="span-8">
   <div id="sidebar-inner">
     <?php
+      echo get_post_meta($post->ID, 'sidebar', true);
       if ($post->post_name != "testimonials") {
         if (function_exists('stray_random_quote')) stray_random_quote('testimonials', false, '', false, 1, 0, 'quoteID', 'ASC');
       }
